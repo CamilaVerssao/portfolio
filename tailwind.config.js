3ed1f10c4a4f9584f858596b40cec0311770cafe
Camila <camila.verssao@gmail.com>
2025-08-23 17:35:28 -0300

    feat: inicio portfolio em react js e tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // garante que o Tailwind funcione em todo o React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
