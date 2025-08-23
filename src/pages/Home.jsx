4fe9d2e470858a4139a798e9068f2d42ee17fb87
Camila <camila.verssao@gmail.com>
2025-08-23 20:17:48 -0300

    feat: criado background de stars + meteors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import ThemeToggle from "@/components/ThemeToggle"
+import StarBackground from "../components/StarBackground"
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme toggle */}
         <ThemeToggle />
-
         {/* Background Effects */}
-
+        <StarBackground />
         {/* Navbar */}
-
         {/* Main Content */}
-
         {/* Footer */}
     </div>
   )
